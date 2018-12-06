93e4451e5068c6c84cf6cc3f4273a7a16d49654f
Cristian <crisgonza2903@gmail.com>
2018-12-06 16:58:10 -0500

    Upload project V1

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+    titulo?: string;
+    email?: string,
+    nombre?: string,
+    apellido?: string,
+    facebook?: string,
+    twitter?: string,
+    instagram?:string
+    equipo_trabajo?: any[];
+}
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+  equipo: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+
+        this.info = resp;
+        this.cargada = true;
+
+        console.log(resp);
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-514b7.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+
+        this.equipo = resp
+        console.log(resp);
+
+        console.log(resp);
+      });
+  }
+}
+
+
